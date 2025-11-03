54a706722f404411e36d7420f036a876b28cc2c8
Mahad Adam <mahad7132@gmail.com>
2025-11-04 03:48:34 +0500

    deployment updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import {ENV} from "./lib/env.js";
 import path from "path";
 const app = express();
+const __dirname = path.resolve();
 app.get("/", (req, res) => {
     res.status(200).json({message: "hey from the server 123"});
 });
