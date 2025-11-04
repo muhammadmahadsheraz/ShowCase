f1a4a3a86965f9e27e60c190480be0feb24a16fe
Mahad Adam <mahad7132@gmail.com>
2025-11-05 02:20:18 +0500

    frontend deployment fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,12 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(cors({origin : ENV.CLIENT_URL,credentials:true}));
 app.use("/api/inngest",serve( {client : inngest , functions}));
-
+if (ENV.NODE_ENV === production){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*",(req,res) =>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+    });
+}
 
 const connectServer =  async () =>{
     try {
